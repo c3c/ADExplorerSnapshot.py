02a58783bea018af5a68bbdac39647820ca4d0b0
c3c <cedric@ce3c.be>
2022-02-10 09:20:49 +0100

    Update test_detectionlab.py

--- tests/test_detectionlab.py
+++ tests/test_detectionlab.py
@@ -4,4 +4,4 @@ import os
 def test_full_parse():
     path = os.path.join(os.path.dirname(__file__), 'data/detectionlab.dat')
     fh = open(path, "rb")
-    adexpsnapshot.ADExplorerSnapshot(fh)
+    adexpsnapshot.ADExplorerSnapshot(fh, '/tmp')
